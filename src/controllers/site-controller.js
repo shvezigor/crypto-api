2aaef583856e696736db562aaf3e14250895641c
igor.shvets <shvezigor@gmail.com>
2023-08-21 23:09:53 +0300

    Init project

--- src/controllers/site-controller.js
+++ src/controllers/site-controller.js
@@ -2,6 +2,6 @@
 
 export const  homePage = async (req, res)=>{
     console.log("get home pages")
-    res.send("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
+    res.send("<html> <head>server Response CRYPTO API</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
     //res.json(data);
 }
