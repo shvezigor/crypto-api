33521dc700ababc7247d3a78a9f0a003367b2c77
igor.shvets <shvezigor@gmail.com>
2023-08-22 09:24:02 +0300

    Init project

--- src/controllers/site-controller.js
+++ src/controllers/site-controller.js
@@ -5,3 +5,9 @@ export const  homePage = async (req, res)=>{
     res.send("<html> <head>server Response CRYPTO API</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
     //res.json(data);
 }
+
+export const  verifyDomain = async (req, res)=>{
+    console.log("domain Verification")
+    res.send("cryptoapis-cb-84f442176185328f40da87be119f35192f5d96a6f0f0e31a0d101f2a758ca87e");
+    //res.json(data);
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,12 @@
 
 import express from 'express';
-import { homePage } from "../controllers/site-controller.js";
+import { homePage, verifyDomain } from "../controllers/site-controller.js";
 //import { getDipLink, fileUpload, sharingDocuments } from '../controllers/diya-controller.js';
 export const router = express.Router();
 
 
 router.get('/', homePage);
-
+router('/cryptoapisverifydomain', verifyDomain);
 //router.get('/api/v1/crypt/getDipLink', getDipLink);
 //router.post('/api/v1/crypt/fileUpload', fileUpload);
 //router.post('/api/v1/crypt/sharingDocuments', sharingDocuments);
