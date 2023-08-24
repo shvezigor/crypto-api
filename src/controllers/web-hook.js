0bab0daac43ef3cf7fe1e6e3a56b5c6195117513
igor.shvets <shvezigor@gmail.com>
2023-08-24 10:55:35 +0300

    Init project

--- src/controllers/web-hook.js
+++ src/controllers/web-hook.js
@@ -3,14 +3,13 @@
 export const  minedTransaction = async (req, res)=>{
     try {
         console.log("minedTransaction", req.body)
-        //console.log("minedTransaction", res)
+        console.log("minedTransaction", req.body.data)
+        console.log("minedTransaction", req.body.data.item.transactionId)
 
-        const response = JSON.parse(req.body);
-        console.log("minedTransaction", response)
 
-        let result = {};
+        //let result = {};
 
-        res.json(result);
+        res.json(req.body.data.item.transactionId);
     }catch (error) {
         console.log(error);
         res.json(error);
