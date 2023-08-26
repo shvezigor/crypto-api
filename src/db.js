736e305af616250d106892f2056638f46aeca695
igor.shvets <shvezigor@gmail.com>
2023-08-26 21:08:18 +0300

    Add work with db. Sync account

--- src/db.js
+++ src/db.js
@@ -6,7 +6,8 @@ dotenv.config();
 var conMySql = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD
+  password: process.env.DATABASE_PASSWORD,
+  port: 3307
 });
 
 // Connect to Data Base
