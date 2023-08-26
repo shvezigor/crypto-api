5b21141ca2d4833ab886caffe0cd4f54926076b7
igor.shvets <shvezigor@gmail.com>
2023-08-26 21:24:26 +0300

    Add work with db. Sync account

--- src/db.js
+++ src/db.js
@@ -7,7 +7,8 @@ var conMySql = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: 'tron'
+  database: 'tron',
+  port:"3306",
 });
 
 // Connect to Data Base
