e40f592685ce5e6e8ffe5d83b4cb4b3790a2a2ef
Ihor <shvezigor@gmail.com>
2024-02-13 12:28:43 +0200

    Add cron.

--- src/xcron.js
+++ src/xcron.js
@@ -1,3 +1,4 @@
+
 import dotenv from 'dotenv';
 import nodeCron from 'node-cron';
 import {getExpiredTime, deletedAccount, getExpiredTimeDeleted} from "./models/account.js";
@@ -5,67 +6,67 @@ import {deleteSubscriptions} from "./api/crypto.js";
 
 dotenv.config();
 
-console.log("Start cron deactivated subscribe by address.")
-const currentDate = new Date();
+nodeCron.schedule('* * * * *', async () => {
+    try {
+       console.log("Start cron deactivated subscribe by address.")
+        const currentDate = new Date();
 
-const year = currentDate.getFullYear();
-const month = String(currentDate.getMonth() + 1).padStart(2, '0');  // Місяці в JavaScript ідуть від 0 до 11
-const day = String(currentDate.getDate()).padStart(2, '0');
-const hours = String(currentDate.getHours()).padStart(2, '0');
-const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');  // Місяці в JavaScript ідуть від 0 до 11
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const hours = String(currentDate.getHours()).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 
-const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-console.log(formattedDate);
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        console.log(formattedDate);
 
-getExpiredTimeDeleted(formattedDate)
-    .then(result => {
-        // Обробка результату, якщо записи існують
-        if (result.length > 0) {
-            // Отримуємо масив обіцянок для всіх викликів deleteSubscriptions
-            const deletePromises = result.map(row => {
-                console.log(`ID: ${row.reference_id}`);
-                // Повертаємо обіцянку для deleteSubscriptions
-                return deleteSubscriptions("tron", "mainnet", row.reference_id)
-                    .then(() => deletedAccount(row.reference_id))
-                    .catch(error => {
-                        console.error(`Помилка при викликанні deleteSubscriptions для ID: ${row.reference_id}`, error);
-                        // Повертаємо обіцянку для deletedAccount в будь-якому випадку
-                        return deletedAccount(row.reference_id);
+        getExpiredTimeDeleted(formattedDate)
+            .then(result => {
+                // Обробка результату, якщо записи існують
+                if (result.length > 0) {
+                    // Отримуємо масив обіцянок для всіх викликів deleteSubscriptions
+                    result.map(row => {
+                        console.log(`ID: ${row.reference_id}`);
+                        // Повертаємо обіцянку для deleteSubscriptions
+                        deleteSubscriptions("tron", "mainnet", row.reference_id)
+                            .then(() => deletedAccount(row.reference_id))
+                            .catch(error => {
+                                console.error(`Помилка при викликанні deleteSubscriptions для ID: ${row.reference_id}`, error);
+                            });
                     });
+                } else {
+                    console.log('Немає записів в результаті запиту.');
+                }
+            })
+            .catch(error => {
+                // Обробка помилки
+                console.error('Помилка при виконанні запиту:', error);
             });
 
-            // Використовуємо Promise.all для очікування виконання всіх deleteSubscriptions та deletedAccount
-            return Promise.all(deletePromises);
-        } else {
-            console.log('Немає записів в результаті запиту.');
-        }
-    })
-    .catch(error => {
-        // Обробка помилки
-        console.error('Помилка при виконанні запиту:', error);
-    });
 
+       /* getExpiredTime(formattedDate)
+            .then(result => {
+                // Обробка результату, якщо записи існують
+                if (result.length > 0) {
+                    result.forEach(row => {
+                        console.log(`ID: ${row.reference_id}`);
+                        deleteSubscriptions("tron", "mainnet", row.reference_id);
+                        deletedAccount(row.reference_id);
+                    });
+                } else {
+                    console.log('Немає записів в результаті запиту.');
+                }
+            })
+            .catch(error => {
+                // Обробка помилки
+                console.error('Помилка при виконанні запиту:', error);
+            });*/
 
-/* getExpiredTime(formattedDate)
-     .then(result => {
-         // Обробка результату, якщо записи існують
-         if (result.length > 0) {
-             result.forEach(row => {
-                 console.log(`ID: ${row.reference_id}`);
-                 deleteSubscriptions("tron", "mainnet", row.reference_id);
-                 deletedAccount(row.reference_id);
-             });
-         } else {
-             console.log('Немає записів в результаті запиту.');
-         }
-     })
-     .catch(error => {
-         // Обробка помилки
-         console.error('Помилка при виконанні запиту:', error);
-     });*/
-
-
+    } catch (e) {
+        console.log(e.message);
+    }
+});
 
   
    
