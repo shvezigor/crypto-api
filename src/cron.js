bbca0a5c5e9628e6e224b8f6db431d1cbba10df0
Ihor <shvezigor@gmail.com>
2024-02-13 12:43:40 +0200

    Add cron.

--- src/cron.js
+++ src/cron.js
@@ -1,80 +1,38 @@
-
 import dotenv from 'dotenv';
 import nodeCron from 'node-cron';
-import {getExpiredTime, deletedAccount} from "./models/account.js";
-import {deleteSubscriptions} from "./api/crypto.js";
+import { getExpiredTime, deletedAccount } from "./models/account.js";
+import { deleteSubscriptions } from "./api/crypto.js";
 
 dotenv.config();
 
 nodeCron.schedule('* * * * *', async () => {
     try {
-       console.log("Start cron deactivated subscribe by address.")
+        console.log("Start cron deactivated subscribe by address.");
+
         const currentDate = new Date();
+        const formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ");
 
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');  // Місяці в JavaScript ідуть від 0 до 11
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const hours = String(currentDate.getHours()).padStart(2, '0');
-        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-
-        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
         console.log(formattedDate);
 
-        getExpiredTime(formattedDate)
-            .then(result => {
-                // Обробка результату, якщо записи існують
-                if (result.length > 0) {
-                    // Отримуємо масив обіцянок для всіх викликів deleteSubscriptions
-                    const deletePromises = result.map(row => {
-                        console.log(`ID: ${row.reference_id}`);
-                        // Повертаємо обіцянку для deleteSubscriptions
-                        return deleteSubscriptions("tron", "mainnet", row.reference_id)
-                            .then(() => deletedAccount(row.reference_id))
-                            .catch(error => {
-                                console.error(`Помилка при викликанні deleteSubscriptions для ID: ${row.reference_id}`, error);
-                                // Повертаємо обіцянку для deletedAccount в будь-якому випадку
-                                return deletedAccount(row.reference_id);
-                            });
-                    });
+        const result = await getExpiredTime(formattedDate);
 
-                    // Використовуємо Promise.all для очікування виконання всіх deleteSubscriptions та deletedAccount
-                    return Promise.all(deletePromises);
-                } else {
-                    console.log('Немає записів в результаті запиту.');
+        if (result.length > 0) {
+            const deletePromises = result.map(async (row) => {
+                try {
+                    console.log(`ID: ${row.reference_id}`);
+                    await deleteSubscriptions("tron", "mainnet", row.reference_id);
+                    await deletedAccount(row.reference_id);
+                } catch (error) {
+                    console.error(`Error processing ID: ${row.reference_id}`, error);
+                    // Continue with the next iteration even if an error occurs
                 }
-            })
-            .catch(error => {
-                // Обробка помилки
-                console.error('Помилка при виконанні запиту:', error);
             });
 
-
-       /* getExpiredTime(formattedDate)
-            .then(result => {
-                // Обробка результату, якщо записи існують
-                if (result.length > 0) {
-                    result.forEach(row => {
-                        console.log(`ID: ${row.reference_id}`);
-                        deleteSubscriptions("tron", "mainnet", row.reference_id);
-                        deletedAccount(row.reference_id);
-                    });
-                } else {
-                    console.log('Немає записів в результаті запиту.');
-                }
-            })
-            .catch(error => {
-                // Обробка помилки
-                console.error('Помилка при виконанні запиту:', error);
-            });*/
-
+            await Promise.all(deletePromises);
+        } else {
+            console.log('No records found in the query result.');
+        }
     } catch (e) {
-        console.log(e.message);
+        console.error(e.message);
     }
 });
-
-  
-   
-
-
-
