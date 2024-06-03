cb1db367dab3c29583d9e6d6e4937a2fce26be95
Ihor <shvezigor@gmail.com>
2024-06-03 22:27:07 +0300

    Add cron.

--- src/pm-server.config.cjs
+++ src/pm-server.config.cjs
@@ -10,6 +10,15 @@ module.exports = {
             cron_restart: '0 * * * *',
         },
         {
+            name: "web-server-crypto-new",
+            script: "./server2.js",
+            instances: 1,
+            watch: true,
+            max_memory_restart: '100M',
+            out_file: "/dev/null",
+            cron_restart: '0 * * * *',
+        },
+        {
             name: "cron-web-crypto",
             script: "./cron.js",
             instances: 1,
